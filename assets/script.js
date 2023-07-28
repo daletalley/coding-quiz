97fe78c88bbd33a5477b19c116b345485edd52fc
Dale Talley III <dtalley32@icloud.com>
2023-07-28 02:11:06 -0500

    answer btn color change

--- assets/script.js
+++ assets/script.js
@@ -224,7 +224,15 @@ function selectAnswer(e) {
     }
 };
     
-
+// answer btn color
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add("correct");
+    } else {
+        element.classList.add("wrong");
+    }
+};
 
 
 
